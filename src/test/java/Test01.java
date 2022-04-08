83370ab8270f59c9d3491765ae2e1eaa9f8db29b
New <2524581939@qq.com>
2022-04-08 11:59:39 +0800

    m test4

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -5,5 +5,6 @@ public class Test01 {
         System.out.println("芜湖哈哈");
         System.out.println("我也开启代码合并冲突");
         System.out.println("代码合并冲突");
+        System.out.println("芜湖1");
     }
 }
