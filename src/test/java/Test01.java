badcf5606460dbc72c8aeb9d1bc7b711b5dcad36
New <2524581939@qq.com>
2022-04-08 11:11:11 +0800

    hot-fix test1

--- src/test/java/Test01.java
+++ src/test/java/Test01.java
@@ -2,6 +2,6 @@ public class Test01 {
     public static void main(String[] args) {
         System.out.println("你是三相琴01");
         System.out.println("你是三相琴02");
-
+        System.out.println("芜湖哈哈");
     }
 }
